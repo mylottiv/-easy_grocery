088e1e49c52b7db6652f754c4c1560f1c1d03b00
Thomas Mylott <mylottiv@gmail.com>
2021-05-21 03:52:40 -0500

    Jest eslint plugin installed

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
     node: true,
+    "jest/globals": true
   },
   extends: [
     'plugin:react/recommended',
@@ -18,8 +19,14 @@ module.exports = {
   },
   plugins: [
     'react',
+    'jest'
   ],
   rules: {
     "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+    "jest/no-disabled-tests": "warn",
+    "jest/no-focused-tests": "error",
+    "jest/no-identical-title": "error",
+    "jest/prefer-to-have-length": "warn",
+    "jest/valid-expect": "error"
   }
 };
