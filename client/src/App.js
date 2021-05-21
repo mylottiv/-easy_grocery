7912fff0b1e4cdde0c7f56fc4335d03450baa482
Thomas Mylott <mylottiv@gmail.com>
2021-05-21 03:54:33 -0500

    After surviving the Bulma Github Issues page, basic mobile first navbar done

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Navbar, Heading, Button } from 'react-bulma-components';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Navbar>
+      <Navbar.Brand>
+        <Navbar.Item href='#'>
+          <Heading weight='bold' size={3}>
+            EZ-Grocery
+          </Heading>
+        </Navbar.Item>
+        <Navbar.Item>
+          <Button color='link'>
+            Login
+          </Button>
+        </Navbar.Item>
+        <Navbar.Burger />
+      </Navbar.Brand>
+      <Navbar.Menu>
+        {/* <Navbar.Container>
+        </Navbar.Container> */}
+      </Navbar.Menu>
+    </Navbar>
+    // <Columns breakpoint='mobile' display='flex' alignItems='stretch' centered vCentered marginless>
+    //   <Columns.Column display='flex' alignItems='center' size={6}>
+    //     <Heading weight='bold' size={4}>
+    //       EZ-Grocery
+    //     </Heading>
+    //   </Columns.Column>
+    //   <Columns.Column display='flex' alignItems='center' size={3}>
+        // <Button color='link'>
+        //   Login
+        // </Button>
+    //   </Columns.Column>
+    //   <Columns.Column display='flex' alignItems='center' size={3}>
+        // <Icon size='large'>
+    //       <img src={logo} alt='hamburger menu'/>
+    //     </Icon>
+    //   </Columns.Column>
+    // </Columns>
+  )
 }
 
 export default App;
--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+it('renders app component', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText("EZ-Grocery")).toBeInTheDocument();
 });
