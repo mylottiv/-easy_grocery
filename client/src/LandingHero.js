5af2ba8a83c7ec7983a73ec2c1a6483b3c80b628
Thomas Mylott <mylottiv@gmail.com>
2021-05-22 08:10:08 -0500

    Formatting basic layout of the landing page hero

--- client/src/LandingHero.js
+++ client/src/LandingHero.js
@@ -1,4 +1,4 @@
-import { Hero, Container, Heading, Box, Columns } from 'react-bulma-components';
+import { Hero, Container, Heading, Box, Columns, Block, Button } from 'react-bulma-components';
 
 function LandingHero() {
     return (
@@ -6,22 +6,25 @@ function LandingHero() {
             <Columns.Column>
                 <Box>
                     <Hero size='halfheight'>
-                        <Hero.Header>
+                        <Hero.Header display='flex' justifyContent='center'>
                             Lorem ipsum
                         </Hero.Header>
                         <Hero.Body>
                             <Container>
-                                <Heading>
+                                <Heading textAlign='center'>
                                     Lorem ipsum dolor sit amet
                                 </Heading>
-                                <Heading
-                                size={3}
-                                >
+                                <Block textAlign='center'>
                                     consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                                </Heading>
+                                </Block>
+                                <Block display='flex' justifyContent='center'>
+                                    <Button color='success'>
+                                        Call To Action
+                                    </Button>
+                                </Block>
                             </Container>
                         </Hero.Body>
-                        <Hero.Footer>
+                        <Hero.Footer display='flex' justifyContent='flex-end'>
                             Massa eget egestas
                         </Hero.Footer>
                     </Hero>
--- client/src/LandingHero.test.js
+++ client/src/LandingHero.test.js
@@ -6,5 +6,6 @@ it('renders landing page hero component', () => {
   expect(screen.getByText("Lorem ipsum")).toBeInTheDocument();
   expect(screen.getByText("Lorem ipsum dolor sit amet")).toBeInTheDocument();
   expect(screen.getByText("consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")).toBeInTheDocument();
+  expect(screen.getByText("Call To Action")).toBeInTheDocument();
   expect(screen.getByText("Massa eget egestas")).toBeInTheDocument();
 });
