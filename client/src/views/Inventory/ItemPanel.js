c392477c82bdd6ffde0b2df5011bd421553d516b
Thomas Mylott <mylottiv@gmail.com>
2021-09-02 13:41:21 -0500

    Fully switched over from tabs to dropdown, some more styling still needed

--- client/src/views/Inventory/ItemPanel.js
+++ client/src/views/Inventory/ItemPanel.js
@@ -1,5 +1,5 @@
 import {useState} from 'react';
-import {Panel, Tabs, Menu, Dropdown, Icon} from 'react-bulma-components';
+import {Panel, Menu, Dropdown, Icon, Level} from 'react-bulma-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
 import ItemSlug from './ItemSlug';
@@ -8,61 +8,30 @@ import dummyItems from './dummyItems';
 function ItemPanel() {
 
     const [selectedTab, setSelectedTab] = useState('Pantry');
-    const tabOnClick = (tabName) => () => setSelectedTab(tabName);
+    const tabOnClick = (tabName) => setSelectedTab(tabName);
 
     return (
         <Panel color='warning'>
             <Panel.Header>
                 Items
             </Panel.Header>
-            {/* need to switch to dropdown style, final "categories" will be determinant on API used */}
-            <Dropdown
-                closeOnSelect
-                color=""
-                icon={<Icon size='small'><FontAwesomeIcon icon={faAngleDown} /></Icon>}
-                label="Dropdown label"
-            >
-                <Dropdown.Item
-                renderAs="a"
-                value="item"
+            {/* final "categories" will be determinant on API used */}
+            <Level justifyContent='center' breakpoint='mobile'>
+                <Dropdown
+                    closeOnSelect
+                    icon={<Icon size='small'><FontAwesomeIcon icon={faAngleDown} /></Icon>}
+                    onChange={tabOnClick}
+                    label={`Category: ${selectedTab}`}
                 >
-                    Dropdown item
-                </Dropdown.Item>
-                <Dropdown.Item
-                renderAs="a"
-                value="other"
-                >
-                    Other Dropdown item
-                </Dropdown.Item>
-                <Dropdown.Item
-                renderAs="a"
-                value="active"
-                >
-                    Active Dropdown item
-                </Dropdown.Item>
-                <Dropdown.Item
-                renderAs="a"
-                value="other 2"
-                >
-                    Other Dropdown item
-                </Dropdown.Item>
-                <Dropdown.Divider />
-                <Dropdown.Item
-                renderAs="a"
-                value="divider"
-                >
-                    After divider
-                </Dropdown.Item>
-            </Dropdown>
-            <Tabs size='small' type='toggle' fullwidth>
-                {Object.keys(dummyItems).map(
-                    (tabName) => (
-                        <Tabs.Tab active={(selectedTab === tabName)} onClick={tabOnClick(tabName)} key={tabName}>
-                            {tabName}
-                        </Tabs.Tab>
-                    )
-                )}
-            </Tabs>
+                    {Object.keys(dummyItems).map(
+                        (tabName) => (
+                            <Dropdown.Item renderAs='a' active={(selectedTab === tabName)} value={tabName} key={tabName}>
+                                {tabName}
+                            </Dropdown.Item>
+                        )
+                    )}
+                </Dropdown>
+            </Level>
             <Menu>
                 <Menu.List>
                     {dummyItems[selectedTab].map(
